6df437ee3cf3bd78c03a78f6c94aa9f7e9ab8d77
SunInJuly <sun_in_july@mail.ru>
2019-04-18 16:56:23 +0300

    fix

--- test_example.py
+++ test_example.py
@@ -1,6 +1,6 @@
 from api import check_lesson_name
 
-def test_():
+def test_check_lesson_name():
     #lesson_url = https://stepik.org/lesson/223529
     lesson_id = 223529
     check_lesson_name(lesson_id=lesson_id, expected_name="wrong_name")
