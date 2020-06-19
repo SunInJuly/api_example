d8bde1c452bf8abd9cd0683e388ef9c7f6d3446c
Iulia Liakh <iulia.liakh@jetbrains.com>
2020-06-19 19:16:05 +0300

    update

--- api.py
+++ api.py
@@ -1,9 +1,7 @@
 import requests
 import json
 
-
 API_LESSONS_URL  = "https://stepik.org/api/lessons/"
-
 
 def check_lesson_name(lesson_id, expected_name):
     response = requests.get(API_LESSONS_URL + str(lesson_id))
