2881d035ea11e8958037f97e07224a490f9a3054
Iulia Liakh <iulia.liakh@jetbrains.com>
2020-06-19 18:50:58 +0300

    api update

--- api.py
+++ api.py
@@ -9,5 +9,7 @@ def check_lesson_name(lesson_id, expected_name):
     response = requests.get(API_LESSONS_URL + str(lesson_id))
     assert response.status_code == 200, "Status code is wrong, expected 200, got {}".format(response.status_code)
     source = json.loads(response.text)
-
+    print(source)
+    actual_name = source['lessons'][0]['title']
+    assert actual_name == expected_name,  "Wrong name, expected {}, got {}".format(expected_name, actual_name)
 
